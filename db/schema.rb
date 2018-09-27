fa0cb1eabfe795e66b94de452df5a89db7245ddb
ArielRodri <ariel-roda@hotmail.com>
2018-09-27 17:41:22 -0500

    adding Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_215744) do
+ActiveRecord::Schema.define(version: 2018_09_27_222844) do
 
   create_table "articles", force: :cascade do |t|
     t.string "tittle"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
