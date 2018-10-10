4dd39605ece8a19346c22a10240280f9a7b5f7b0
ArielRodri <ariel-roda@hotmail.com>
2018-10-10 17:42:31 -0500

    Adding validations to the model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+	belongs_to :user
+	validates :title, uniqueness: true
+	validates :title,:body,presence: true
+	validates :title, length:{in 5..25}
+	validates :body, length: {minimun: 5, too_short: "Maximo son %{count`caracteres." }
 end
