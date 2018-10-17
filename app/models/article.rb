a254ac05bf0a2af5708a0104661be3ac7e195481
ArielRodri <ariel-roda@hotmail.com>
2018-10-17 17:56:12 -0500

    Showing the articles of a category

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,10 +7,10 @@ class Article < ApplicationRecord
 	validates :title, uniqueness: true
 	validates :title,:body,presence: true
 	validates :title, length:{in 5..25}
-	validates :body, length: {minimun: 50, too_short: "Minimo son %{count`caracteres." }
+	validates :body, length: {minimun: 450, too_short: "Minimo son %{count`caracteres." }
 	def categories= (value)
 		@categories = value
-		#S<raise value.to_yaml
+		#raise value.to_yaml
 	end
 	private
 	def save_categories
